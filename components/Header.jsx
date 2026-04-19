17fc14d8973e540a4305b8310de0c16d4bb48cb6
vinayak-dhar <vinayakdhar003@gmail.com>
2026-04-19 23:39:45 +0530

    add Clerk auth UI to header
    - integrated SignInButton for signed-out users
    - added UserButton with custo styling for signed-in users
    - implemented signed-in and signed-out conditional rendering
    - improved header authentication flow and UX

---
+++ components/Header.jsx
@@ -0,0 +1,164 @@
+import React from 'react'
+import { Button } from "./ui/button";
+import {
+    Calendar,
+    CreditCard,
+    ShieldCheck,
+    Stethoscope,
+    User,
+} from "lucide-react";
+import Link from "next/link";
+import { Show, SignInButton, UserButton } from "@clerk/nextjs";
+import { Badge } from './ui/badge';
+import Image from "next/image";
+import { checkUser } from '@/lib/checkUser';
+import { checkAndAllocateCredits } from "@/actions/credits";
+
+export default async function Header() {
+
+    const user = await checkUser();
+    if (user?.role === "PATIENT") {
+        await checkAndAllocateCredits(user);
+    }
+    
+    return (
+        <header className='fixed top-0 w-full border-b bg-background/90 backdrop-blur-md z-30 supports-[backdrop-filter]:bg-background/80'>
+            <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
+                <Link href="/" className=''>
+                    <Image
+                        src="/logo112.png"
+                        alt="EduConnect Logo"
+                        width={200}
+                        height={60}
+                        className='h-30 w-auto object-contain'
+                    />
+                </Link>
+
+                {/* Action Buttons */}
+                {/* like the user logged in to our website 
+                    so if the user didn't decide anything - so we will redirect the user to sign in page
+                */}
+                <div className='flex items-center space-x-2'>
+                    <Show when="signed-in">
+                        {/* Admin Links */}
+
+                        {/* admin user - so it will need admin dashboard */}
+                        {user?.role === "ADMIN" && (
+                            <Link href="/admin">
+                                <Button
+                                    variant='outline'
+                                    className="hidden md:inline-flex items-center gap-2"
+                                >
+                                    <ShieldCheck className='h-4 w-4' />
+                                    Admin Dashboard
+                                </Button>
+                                <Button variant='ghost' className='md:hidden w-10 h-10 p-0'>
+                                    <ShieldCheck className='h-4 w-4' />
+                                </Button>
+                            </Link>
+                        )}
+
+
+                        {/* Mentor Links */}
+                        {user?.role === "DOCTOR" && (
+                            <Link href='/doctor'>
+                                <Button
+                                    variant='outline'
+                                    className='hidden md:inline-flex items-center gap-2'
+                                >
+                                    <Stethoscope className='h-4 w-4' />
+                                    Mentor Dashboard
+                                </Button>
+                                <Button variant='ghost' className='md:hidden w-10 h-10 p-0'>
+                                    <Stethoscope className='h-4 w-4' />
+                                </Button>
+                            </Link>
+                        )}
+                        
+                        {/* if user role is student - we will take the user to appointment page */}
+                        {/* Student Links */}
+                        {user?.role === "PATIENT" && (
+                            <Link href="/appointments">
+                                <Button
+                                    variant='outline'
+                                    className="hidden md:inline-flex items-center gap-2"
+                                >
+                                    <ShieldCheck className='h-4 w-4' />
+                                    {/* only for students */}
+                                    My Appointments
+                                </Button>
+                                <Button variant='ghost' className='md:hidden w-10 h-10 p-0'>
+                                    <ShieldCheck className='h-4 w-4' />
+                                </Button>
+                            </Link>
+                        )}
+                        
+                        
+                        
+                        {/* Unassigned Role */}
+                        {user?.role === "UNASSIGNED" && (
+                            <Link href="/onboarding">
+                                <Button
+                                    variant='outline'
+                                    className="hidden md:inline-flex items-center gap-2"
+                                >
+                                    <ShieldCheck className='h-4 w-4' />
+                                    Complete Profile
+                                </Button>
+                                <Button variant='ghost' className='md:hidden w-10 h-10 p-0'>
+                                    <ShieldCheck className='h-4 w-4' />
+                                </Button>
+                            </Link>
+                        )}
+                    </Show>
+
+
+                    {(!user || user?.role !== "ADMIN") && (
+                        <Link href={user?.role === "PATIENT" ? "/pricing" : "/doctor"}>
+                            <Badge
+                                variant='outline'
+                                className='h-9 bg-emerald-900/20 border-emerald-700/30 px-3 py-1 flex items-center gap-2'
+                            >
+                                <CreditCard className='' />
+                                <span className=''>
+                                    {user && user.role !== "ADMIN" ? (
+                                        <>
+                                            {user.credits}{" "}
+                                            <span className=''>
+                                                {user?.role === "PATIENT"
+                                                    ? "Credits" 
+                                                    : "Earned Credits"
+                                                }
+                                            </span>
+                                        </>
+                                    ) : (
+                                        <>Pricing</>
+                                    )}
+                                </span>
+                            </Badge>
+                        </Link>
+                    )}
+
+                    <Show when="signed-out">
+                        <SignInButton>
+                            <Button variant='secondary'>Sign In</Button>
+                        </SignInButton>
+                    </Show>
+
+                    <Show when="signed-in">
+                        <UserButton 
+                            appearance={{
+                                elements: {
+                                    avatarBox: "w-10 h-10",
+                                    userButtonPopoverCard: "shadow-xl",
+                                    userPreviewMainIdentifier: "font-semibold",
+                                },
+                            }}
+                            afterSignOutUrl="/"
+                        />
+                    </Show>
+                </div>
+            </nav>
+        </header>
+    );
+}
